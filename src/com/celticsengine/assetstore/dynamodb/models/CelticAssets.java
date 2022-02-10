5edb84a2c1432189773334b7e3665f6515130a51
William <vas069@icloud.com>
2022-02-10 15:12:57 -0800

    Auto stash before merge of "develop" and "origin/develop"
    Update

---
+++ src/com/celticsengine/assetstore/dynamodb/models/CelticAssets.java
@@ -0,0 +1,113 @@
+package com.celticsengine.assetstore.dynamodb.models;
+
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBAttribute;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBHashKey;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBRangeKey;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBTable;
+
+import java.util.Set;
+
+@DynamoDBTable(tableName = "celtic_assets")
+public class CelticAssets {
+
+   private String userId;
+   private String assetId;
+   private String name ;
+   private String assetLocation;
+   private String discription;
+   private Set<String> images;
+   private double fileSize;
+   private String downloads;
+   private String compatableEngineVer;
+   private String datePosted;
+
+   @DynamoDBHashKey(attributeName = "user_id")
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    @DynamoDBRangeKey(attributeName = "asset_id")
+    public String getAssetId() {
+        return assetId;
+    }
+
+    public void setAssetId(String assetId) {
+        this.assetId = assetId;
+    }
+
+    @DynamoDBAttribute(attributeName = "name")
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @DynamoDBAttribute(attributeName = "asset_location")
+    public String getAssetLocation() {
+        return assetLocation;
+    }
+
+    public void setAssetLocation(String assetLocation) {
+        this.assetLocation = assetLocation;
+    }
+
+    @DynamoDBAttribute(attributeName = "description")
+    public String getDiscription() {
+        return discription;
+    }
+
+    public void setDiscription(String discription) {
+        this.discription = discription;
+    }
+
+    @DynamoDBAttribute(attributeName = "images")
+    public Set<String> getImages() {
+        return images;
+    }
+
+    public void setImages(Set<String> images) {
+        this.images = images;
+    }
+
+    @DynamoDBAttribute(attributeName = "file_size")
+    public double getFileSize() {
+        return fileSize;
+    }
+
+    public void setFileSize(double fileSize) {
+        this.fileSize = fileSize;
+    }
+
+    @DynamoDBAttribute(attributeName = "downloads")
+    public String getDownloads() {
+        return downloads;
+    }
+
+    public void setDownloads(String downloads) {
+        this.downloads = downloads;
+    }
+
+    @DynamoDBAttribute(attributeName = "compatable_engine_ver")
+    public String getCompatableEngineVer() {
+        return compatableEngineVer;
+    }
+
+    public void setCompatableEngineVer(String compatableEngineVer) {
+        this.compatableEngineVer = compatableEngineVer;
+    }
+
+    @DynamoDBAttribute(attributeName = "date_created")
+    public String getDatePosted() {
+        return datePosted;
+    }
+
+    public void setDatePosted(String datePosted) {
+        this.datePosted = datePosted;
+    }
+}
